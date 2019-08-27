3af17f13f319dec25afcaaaf547996eaff15b251
Alex Mchugh <alexmchugh@Alexs-MacBook-Pro.local>
2019-08-27 19:13:00 -0400

    Removed duplicate code

--- day12_2.py
+++ day12_2.py
@@ -1,7 +1,6 @@
 import json
 import sys
 
-
 with open(sys.argv[1]) as json_file:
     total = 0
     data = json.load(json_file)
@@ -9,23 +8,18 @@ with open(sys.argv[1]) as json_file:
     while(queue):
         to_add = []
         for el in queue:
+            vals = None
             if type(el) == dict:
                 vals = el.values()
                 if "red" in vals:
                     continue
-                else:
-                    for el2 in vals:
-                        if type(el2) == list or type(el2) == dict:
-                            to_add.append(el2)
-                        else:
-                            if type(el2) == int:
-                                total += el2
             elif type(el) == list:
-                for el2 in el:
-                    if type(el2) == list or type(el2) == dict:
-                        to_add.append(el2)
-                    else:
-                        if type(el2) == int:
-                            total += el2
+                vals = el
+            for el2 in vals:
+                if type(el2) == list or type(el2) == dict:
+                    to_add.append(el2)
+                else:
+                    if type(el2) == int:
+                        total += el2
         queue = to_add
     print(total)
